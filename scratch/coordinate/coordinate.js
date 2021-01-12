8812be54b4da7adbfb4aebf983587e89b71164d2
Ivan <vanger.char@gmail.com>
2021-01-12 16:14:36 +0300

    update coordite Scratch XY

--- scratch/coordinate/coordinate.js
+++ scratch/coordinate/coordinate.js
@@ -1,12 +1,16 @@
 const coorX = document.getElementById('coorX'),
     coorY = document.getElementById('coorY'),
+    toX = document.getElementById('toX'),
+    toY = document.getElementById('toY'),
     cat = document.getElementById('cat');
 
 function setY (){
     let y = +coorY.value + 180;
     cat.style.bottom = y + "px";
+    toY.textContent = coorY.value;
 }
 function setX (){
     let x = +coorX.value + 240;
     cat.style.left = x + "px";
+    toX.textContent = coorX.value;
 }
